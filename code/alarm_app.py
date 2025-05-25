e1fc5f96452822349f55068d34890438c2c3acee
simjunhyun <junhunsim@naver.com>
2025-05-25 22:32:48 +0900

    Update alarm_app.py

--- code/alarm_app.py
+++ code/alarm_app.py
@@ -21,3 +21,26 @@ class AlarmApp:
 if __name__ == "__main__":
     app = AlarmApp()
     app.add_alarm(7, 30, ['월', '화', '수', '목', '금'])
+class Alarm:
+    def __init__(self, hour, minute, repeat_days):
+        self.hour = hour
+        self.minute = minute
+        self.repeat_days = repeat_days
+
+    def save(self):
+        print(f"알람이 {self.hour}시 {self.minute}분에 설정되었습니다. 반복 요일: {', '.join(self.repeat_days)}")
+
+
+class AlarmApp:
+    def __init__(self):
+        self.alarms = []
+
+    def add_alarm(self, hour, minute, repeat_days):
+        alarm = Alarm(hour, minute, repeat_days)
+        alarm.save()
+        self.alarms.append(alarm)
+
+
+if __name__ == "__main__":
+    app = AlarmApp()
+    app.add_alarm(7, 30, ['월', '화', '수', '목', '금'])
